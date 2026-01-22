7cd478cc568c483c361aa931a42a78a51df0cff1
Abdelrahman <abdorahmany2004@gmail.com>
2026-01-22 14:53:04 +0200

    id

--- Project.Infrastructure/ApplicationDbContext/Test.cs
+++ Project.Infrastructure/ApplicationDbContext/Test.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project.Infrastructure.ApplicationDbContext;
 
 public partial class Test
 {
+    [Key]
+    public int Id { get; set; }
     public string? Test1 { get; set; }
 }
