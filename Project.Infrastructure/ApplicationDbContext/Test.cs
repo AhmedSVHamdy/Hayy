3daa696e2233c6c35bd6ee66afdb93647df5e27a
Ahmed Mohamed Taha <120645491+AhmedSVHamdy@users.noreply.github.com>
2026-01-22 14:47:06 +0200

    DbContext

---
+++ Project.Infrastructure/ApplicationDbContext/Test.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project.Infrastructure.ApplicationDbContext;
+
+public partial class Test
+{
+    public string? Test1 { get; set; }
+}
