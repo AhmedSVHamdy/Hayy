2134f60744885bb25d6c007606294e7b081c6c08
Abdelrahman <abdorahmany2004@gmail.com>
2026-04-15 23:42:58 +0200

    Return Id in Login Web

--- Project.Core/DTO/AuthenticationResponse.cs
+++ Project.Core/DTO/AuthenticationResponse.cs
@@ -3,6 +3,7 @@
     public class AuthenticationResponse
     {
         public string? PersonName { get; set; } = string.Empty;
+        public Guid? Id { get; set; } 
         public string? Email { get; set; } = string.Empty;
         public string? Token { get; set; } = string.Empty;
         public DateTime Expiration { get; set; }
